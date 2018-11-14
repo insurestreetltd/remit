18e21055f04042798f597a6fd59c8e0749e125e4
Jason Morris <lemnik@gmail.com>
2018-11-14 12:35:34 +0000

    Fixed the TS definition

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,57 @@
 import {Connection} from "amqplib";
 
-declare module "remit" {
-  interface RemitOptions {
+interface RemitConstructorOptions {
     exchange?: string;
     name?: string;
     url?: string;
     connection?: Connection;
-  }
+}
 
-  function remit(options?: RemitOptions): remit.Remit;
+declare function remit(options?: RemitConstructorOptions): remit.Remit;
 
-  namespace remit {
+declare namespace remit {
     export interface Event {
-      data?: any;
-      error?: Error;
-      status?: boolean;
+        data?: any;
+        error?: Error;
+        status?: boolean;
     }
 
     export type Handler = (event: Event) => void;
 
     export interface Endpoint {
-      handler(...handlers: Handler[]): Endpoint;
+        handler(...handlers: Handler[]): Endpoint;
 
-      pause(cold: boolean): Promise<Endpoint>;
+        pause(cold: boolean): Promise<Endpoint>;
 
-      resume(): Promise<Endpoint>;
+        resume(): Promise<Endpoint>;
 
-      start(): Promise<void>;
+        start(): Promise<void>;
     }
 
     export interface Listener {
-      handler(...handlers: Handler[]): Listener;
+        handler(...handlers: Handler[]): Listener;
 
-      start(): Promise<void>;
+        start(): Promise<void>;
 
-      stop(): Promise<void>;
+        stop(): Promise<void>;
     }
 
     export type Emitter = (data: any) => Promise<void>;
 
     export interface Remit {
-      request(endpointName: string): (args?: any) => Promise<any>;
+        request(endpointName: string): (args?: any) => Promise<any>;
 
-      endpoint(endpointName: string, handler: Handler): Endpoint;
+        endpoint(endpointName: string, handler: Handler): Endpoint;
 
-      listen(opts: {
-        event: string;
-        queue?: string;
-        prefetch?: number;
-        subscribe?: boolean;
-      }): Listener;
+        listen(opts: {
+            event: string;
+            queue?: string;
+            prefetch?: number;
+            subscribe?: boolean;
+        }): Listener;
 
-      emit(opts: {event: string; delay?: number}): Emitter;
+        emit(opts: {event: string; delay?: number}): Emitter;
     }
-  }
-
-  export = remit;
 }
+
+export = remit;
